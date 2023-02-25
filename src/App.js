4b85e258878545e03df849018adf60c55ed59474
Ben Yoon <BYoon12@gmail.com>
2023-02-25 10:18:15 -0500

    completed hw

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { useState, useRef } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
 import { DataContext } from './context/DataContext';
 import { SearchContext } from './context/SearchContext';
+import AlbumView from './components/AlbumView';
+import ArtistView from './components/ArtistView';
+import { Fragment } from "react";
 import "./App.css";
 
 function App() {
@@ -27,21 +31,25 @@ function App() {
     };
     fetchData();
   };
-
+  
   return (
-    <div className="App">
-      <SearchContext.Provider value ={{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-      <SearchBar/>
-      </SearchContext.Provider>
+    <div>
       {message}
-      <DataContext.Provider value={data} >
-      <Gallery /> 
-      </DataContext.Provider>
+        <Router>
+          <Routes>
+            <Route path="/" element={
+              <Fragment>
+                <SearchBar handleSearch = {handleSearch}/>
+                <Gallery data={data}/>
+              </Fragment>
+            } />
+            <Route path="/album/:id" element={<AlbumView />} />
+            <Route path="/artist/:id" element={<ArtistView />} />
+          </Routes>
+        </Router>
     </div>
-  );
+  )
+
 }
 
 export default App;
